4e6dc7e8c5f9b4ed3cdea78a25d8b7f008641133
MomenBahgat <momen.emc2@gmail.com>
2015-09-26 04:06:42 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snack.master;
+
 import javax.swing.JFrame;
 
+/**
+ *
+ * @author Amoula
+ */
 public class Main {
 
-	public static void main(String[] args) {
-
-		//Creating the window with all its awesome snaky features
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        	//Creating the window with all its awesome snaky features
 		Window f1= new Window();
 		
 		//Setting up the window settings
 		f1.setTitle("Snake");
-		f1.setSize(300,300);
+		f1.setSize(500,300);
 		f1.setVisible(true);
 		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
 
-	}
+    }
+    
 }
